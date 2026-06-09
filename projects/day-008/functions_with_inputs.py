b3d68089d5e1031d310b38c681c59cebfa401471
kiwisigma <kiwisigma@users.noreply.github.com>
2026-06-09 08:27:28 -0500

    progress update — 2026-06-09 08:27 (1 files)

--- projects/day-008/functions_with_inputs.py
+++ projects/day-008/functions_with_inputs.py
@@ -8,8 +8,19 @@
 #
 # Functions that allows for inputs
 
-def greet_with_name(name):
-    print(f"Hello {name}")
-    print(f"How are you {name}?")
+# def greet_with_name(name):
+#     print(f"Hello {name}")
+#     print(f"How are you {name}?")
+#
+# greet_with_name("Billie")
+# Functions with more than 1 input
 
-greet_with_name("Billie")
+def greet_with(name, location):
+    print(f"Hello {name}!")
+    print(f"What is it like in {location}?")
+#
+# greet_with("Jack Bauer", "Nowhere")
+# # Positional argument
+# greet_with("Nowhere", "Jack Bauer")
+# Assigned keywords
+greet_with(name="Angela", location="London")
