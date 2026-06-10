4c5e36e569cc6ae9ae3488ef30503e96710a293f
kiwisigma <kiwisigma@users.noreply.github.com>
2026-06-10 12:39:40 -0500

    progress update — 2026-06-10 12:39 (1 files)

--- projects/day-008/Caesar_Cipher.py
+++ projects/day-008/Caesar_Cipher.py
@@ -24,5 +24,17 @@ def decrypt(original_text, shift_amount):
         output_text += alphabet[shifted_position]
     print(f"Here is the encoded result: {output_text}")
 
+def caesar(original_text, shift_amount, encode_or_decode):
+    output_text = ""
+    for letter in original_text:
+
+        if encode_or_decode == "decode":
+            shift_amount *= -1
+
+        shifted_position = alphabet.index(letter) + shift_amount
+        shifted_position %= len(alphabet)
+        output_text += alphabet[shifted_position]
+    print(f"Here is the encoded result: {output_text}")
+
 decrypt(original_text=text, shift_amount=shift)
 # encrypt(original_text=text, shift_amount=shift)
