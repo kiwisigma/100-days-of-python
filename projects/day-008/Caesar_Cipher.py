64498e9bf40dba1ce531562801cc28d1a46367d4
kiwisigma <kiwisigma@users.noreply.github.com>
2026-06-10 12:49:18 -0500

    progress update — 2026-06-10 12:49 (2 files)

--- projects/day-008/Caesar_Cipher.py
+++ projects/day-008/Caesar_Cipher.py
@@ -1,5 +1,6 @@
+import art
+print(art.logo)
 alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm','n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
-
 direction = input(f"Type 'encode' to encrypt, type 'decode' to decrypt:\n").lower()
 text = input("Type your message:\n").lower()
 shift = int(input("Type the shift number:\n"))
