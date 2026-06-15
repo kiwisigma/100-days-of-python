ddaa36afe45900b2669c4af270926963f0701344
kiwisigma <kiwisigma@users.noreply.github.com>
2026-06-15 17:52:02 -0500

    progress update — 2026-06-15 17:52 (1 files)

--- projects/day-011/blackjack.py
+++ projects/day-011/blackjack.py
@@ -1,10 +1,8 @@
-# import art
+import art
 import random
-# from art import logo
-# print(logo)
-# #
-# print(f"Do you want to play a game of Blackjack? type 'y' or 'n':")
-# 
+from art import logo
+print(logo)
+
 def deal_card():
     """returns a random card from the deck"""
     cards = [11, 2, 3, 4, 5, 6, 7, 8, 9, 10, 10, 10, 10]
@@ -20,18 +18,57 @@ def calculate_score(cards):
 
     return sum(cards)
 
-user_cards = []
-computer_cards = []
-is_game_over = False
+def compare(user_score, computer_score):
+    if user_score == computer_score:
+        return "Draw "
+    elif computer_score == 0:
+        return "Lose, opponent has Blackjack"
+    elif user_score == 0:
+        return "Win with a Blackjack!"
+    elif user_score > 21:
+        return "You went over. You Lose"
+    elif computer_score > 21:
+        return "Opponent went over. You win"
+    elif user_score > computer_score:
+        return "You win!"
+    else:
+        return "You lose"
 
-for _ in range(2):
-    user_cards.append(deal_card())
-    computer_cards.append(deal_card())
+def play_game():
+    print(art.logo)
+    user_cards = []
+    computer_cards = []
+    computer_score = -1
+    user_score = -1
+    is_game_over = False
 
-user_score = calculate_score(user_cards)
-computer_score = calculate_score(computer_cards)
-print(f"Your cards: {user_cards}, current score: {user_score}")
-print(f"Computer's first cards: {computer_cards[0]}")
+    for _ in range(2):
+        user_cards.append(deal_card())
+        computer_cards.append(deal_card())
 
-if user_score == 0 or computer_score == 0 or user_score > 21:
-    is_game_over = True
+    while not is_game_over:
+        user_score = calculate_score(user_cards)
+        computer_score = calculate_score(computer_cards)
+        print(f"Your cards: {user_cards}, current score: {user_score}")
+        print(f"Computer's first cards: {computer_cards[0]}")
+
+        if user_score == 0 or computer_score == 0 or user_score > 21:
+            is_game_over = True
+        else:
+            user_should_deal = input("Type 'y' to get another card, type 'n' to pass: ")
+            if user_should_deal == "y":
+                user_cards.append(deal_card())
+            else:
+                is_game_over = True
+
+    while computer_score != 0 and computer_score < 17:
+        computer_cards.append(deal_card())
+        computer_score = calculate_score(computer_cards)
+
+    print(f"Your final hand: {user_cards}, final score: {user_score}")
+    print(f"Computer's final hand: {computer_cards}, final score: {computer_score}")
+    print(compare(user_score, computer_score))
+
+while input("Do you want to play a game of Blackjack? Type 'y' or 'n': ") == "y":
+    print("\n" *20)
+    play_game()
