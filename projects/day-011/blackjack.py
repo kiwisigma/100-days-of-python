73b45317b336e11b774830d470d2d5a07095f7b2
kiwisigma <kiwisigma@users.noreply.github.com>
2026-06-15 16:54:34 -0500

    progress update — 2026-06-15 16:54 (1 files)

--- projects/day-011/blackjack.py
+++ projects/day-011/blackjack.py
@@ -5,15 +5,14 @@ import random
 # #
 # print(f"Do you want to play a game of Blackjack? type 'y' or 'n':")
 # 
-def deal_cards():
+def deal_card():
     """returns a random card from the deck"""
     cards = [11, 2, 3, 4, 5, 6, 7, 8, 9, 10, 10, 10, 10]
-    random.choice(cards)
     return random.choice(cards)
 
 user_cards = []
 computer_cards = []
 
 for _ in range(2):
-    new_card = deal_cards()
-    user_cards.append(new_card)
+    user_cards.append(deal_card())
+    computer_cards.append(deal_card())
