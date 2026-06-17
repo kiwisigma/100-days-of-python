59ae1e69443b31ee943506aa018055769337e5b9
kiwisigma <kiwisigma@users.noreply.github.com>
2026-06-17 07:09:47 -0500

    progress update — 2026-06-17 07:09 (1 files)

---
+++ projects/day-012/drill2.py
@@ -0,0 +1,10 @@
+#Store an input, return based on it
+
+def get_color():
+    color = input(f"What is your favorite color?:\n")
+    if color == "red" or color == "orange" or color == "yellow":
+        return "warm"
+    else:
+        return "cool"
+
+print(get_color())
