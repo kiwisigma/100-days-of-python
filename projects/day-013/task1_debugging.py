f774f605215899afac11f0c546a2ad3cc3018144
kiwisigma <kiwisigma@users.noreply.github.com>
2026-06-18 04:38:32 -0500

    progress update — 2026-06-18 04:38 (4 files)

---
+++ projects/day-013/task1_debugging.py
@@ -0,0 +1,15 @@
+def my_function():
+    for i in range(1, 20):
+        if i == 20:
+            print("You got it")
+
+my_function()
+
+# Describe the Problem - Write your answer as comments:
+#1. What is the for loop doing?
+"""the for loop is cycling 1-20 and printing "You got this if i is == to 20"."""
+#2. When is the function meant to print "You got it"?
+"""when i == 20"""
+#3. What are your assumptions about the value of i?
+"""if a print statement is executed it must equal 20, if no print statement has been executed
+i is not equal to 20"""
---
+++ projects/day-013/task2.py
@@ -0,0 +1,4 @@
+from random import randint
+dice_images = ["⚀", "⚁", "⚂", "⚃", "⚄", "⚅"]
+dice_num = randint(1, 6)
+print(dice_images[dice_num])
