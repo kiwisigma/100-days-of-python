c547e9d8f81f9968545e2464ee4c463463dfdae9
kiwisigma <kiwisigma@users.noreply.github.com>
2026-06-21 11:34:04 -0500

    progress update — 2026-06-21 11:34 (2 files)

--- projects/day-016/OOP.py
+++ projects/day-016/OOP.py
@@ -1,17 +1,23 @@
 import turtle
-
 import another_module
 print(another_module.another_variable)
 
-from turtle import Turtle, Screen
-timmy = Turtle()
-print(timmy)
-timmy.shape("turtle")
-timmy.color("red", "green")
-timmy.forward(100)
-timmy.position()
-(100, 0.00)
+# from turtle import Turtle, Screen
+# timmy = Turtle()
+# print(timmy)
+# timmy.shape("turtle")
+# timmy.color("red", "green")
+# timmy.forward(100)
+# timmy.position()
+# (100, 0.00)
+#
+# my_screen = Screen()
+# print(my_screen.canvheight)
+# my_screen.exitonclick()
 
-my_screen = Screen()
-print(my_screen.canvheight)
-my_screen.exitonclick()
+from prettytable import PrettyTable
+
+table = PrettyTable()
+table.field_names = ["City name", "Area", "Population", "Annual Rainfall"]
+table.add_row(["Adelaide", 1295, 1158259, 600.5])
+print(table)
