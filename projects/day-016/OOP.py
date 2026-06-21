84350088cba8d13be3a2b0018712131ec8d30653
kiwisigma <kiwisigma@users.noreply.github.com>
2026-06-21 10:27:47 -0500

    progress update — 2026-06-21 10:27 (1 files)

--- projects/day-016/OOP.py
+++ projects/day-016/OOP.py
@@ -6,7 +6,9 @@ print(another_module.another_variable)
 from turtle import Turtle, Screen
 timmy = Turtle()
 print(timmy)
+timmy.shape("turtle")
+timmy.color("red", "green")
 
 my_screen = Screen()
 print(my_screen.canvheight)
-
+my_screen.exitonclick()
