c016dcd90b347ea8f51f834c17c06a4220ebfd56
kiwisigma <kiwisigma@users.noreply.github.com>
2026-06-24 06:06:14 -0500

    progress update — 2026-06-24 06:06 (1 files)

--- projects/day-016/coffee-machine/main.py
+++ projects/day-016/coffee-machine/main.py
@@ -1,3 +1,14 @@
 from menu import Menu
 from coffee_maker import CoffeeMaker
 from money_machine import MoneyMachine
+
+menu = Menu()
+coffee_maker = CoffeeMaker()
+money_machine = MoneyMachine()
+is_on = True
+
+money_machine.report()
+coffee_maker.report()
+
+while is_on:
+    options = menu.get_items()
