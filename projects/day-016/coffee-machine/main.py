e8a759ca9fd23759d10f86f6e363c5a3a4951367
kiwisigma <kiwisigma@users.noreply.github.com>
2026-06-24 06:09:08 -0500

    progress update — 2026-06-24 06:09 (1 files)

--- projects/day-016/coffee-machine/main.py
+++ projects/day-016/coffee-machine/main.py
@@ -12,3 +12,12 @@ coffee_maker.report()
 
 while is_on:
     options = menu.get_items()
+    choice = input(f"What would you like? ({options}): ")
+    if choice == "off":
+        is_on = False
+    elif choice == "report":
+        coffee_maker.report()
+        money_machine.report()
+    else:
+        drink = menu.find_drink(choice)
+        print(drink)
