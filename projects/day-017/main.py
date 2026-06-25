4c2de4f61c94fdddb157e8f17f6e558af6044944
kiwisigma <kiwisigma@users.noreply.github.com>
2026-06-25 13:47:56 -0500

    progress update — 2026-06-25 13:47 (1 files)

--- projects/day-017/main.py
+++ projects/day-017/main.py
@@ -2,12 +2,13 @@ class User:
     def __init__(self, user_id, username):
         self.id = user_id
         self.username = username
+        self.followers = 0
+
 
 user_1 = User("001", "Angela")
+user_2 = User("002", "jack")
+print(user_1.followers)
 
-print(user_1.id)
-#
-#
 # user_2 = User()
 # user_2.id = "002"
 # user_2.username = "jack"
