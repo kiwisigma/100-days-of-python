bef7a89e6c0694b5ce9d9ae3c69385dae1d02431
kiwisigma <kiwisigma@users.noreply.github.com>
2026-06-25 13:41:49 -0500

    progress update — 2026-06-25 13:41 (1 files)

--- projects/day-017/main.py
+++ projects/day-017/main.py
@@ -1,8 +1,13 @@
 class User:
-    pass
+    def __init__(self):
+        print("new user being created...")
 
 user_1 = User()
 user_1.id = "001"
 user_1.username = "angela"
 
 print(user_1.username)
+
+user_2 = User()
+user_2.id = "002"
+user_2.username = "jack"
