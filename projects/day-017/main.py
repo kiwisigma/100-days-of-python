d89357a10efc7367fa3dc66d0134f974d9b5837b
kiwisigma <kiwisigma@users.noreply.github.com>
2026-06-25 13:45:31 -0500

    progress update — 2026-06-25 13:45 (1 files)

--- projects/day-017/main.py
+++ projects/day-017/main.py
@@ -1,13 +1,13 @@
 class User:
-    def __init__(self):
-        print("new user being created...")
+    def __init__(self, user_id, username):
+        self.id = user_id
+        self.username = username
 
-user_1 = User()
-user_1.id = "001"
-user_1.username = "angela"
+user_1 = User("001", "Angela")
 
-print(user_1.username)
-
-user_2 = User()
-user_2.id = "002"
-user_2.username = "jack"
+print(user_1.id)
+#
+#
+# user_2 = User()
+# user_2.id = "002"
+# user_2.username = "jack"
